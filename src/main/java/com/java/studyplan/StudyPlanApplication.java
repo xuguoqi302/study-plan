47ffabe4edef574cbedf2f885166e72751c58eab
xuguoqi <guoqixu@huashenghaoche.com>
2019-09-22 14:24:39 +0800

    Initial commit

---
+++ src/main/java/com/java/studyplan/StudyPlanApplication.java
@@ -0,0 +1,13 @@
+package com.java.studyplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPlanApplication.class, args);
+	}
+
+}
